eb8b633a225c5487ea22926a58d19d1a3ff2632e
felixDev22 <felixnzomo@gmail.com>
2023-05-03 18:43:13 +0300

    Add user sign-out path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+ root "splash#index"
   devise_for :users
   resources :user
   resources :categories
   resources :transactions
-  root "splash#index"
+  
+   devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
 end
