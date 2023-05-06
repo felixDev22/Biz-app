8110f0804da87a4b6f0031022246d2e6e68bbf08
felixDev22 <felixnzomo@gmail.com>
2023-05-06 15:49:28 +0300

    Fix linters errors

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,3 @@
-
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
@@ -6,7 +5,7 @@ RSpec.describe Category, type: :model do
     before(:each) do
       first_user = User.create!(name: 'Felix', email: 'test@gmail.com', password: 'test12345')
       icon = fixture_file_upload(Rails.root.join('spec/fixtures/icons/uber.png'), 'image/png')
-      @category = Category.create(name: 'Travel', icon: icon, user_id: first_user.id)
+      @category = Category.create(name: 'Travel', icon:, user_id: first_user.id)
     end
 
     it 'is created successfully' do
