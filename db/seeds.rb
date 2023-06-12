7b21ca1bf7204b19421e22a3ea7424904359c7a9
felixDev22 <felixnzomo@gmail.com>
2023-06-12 23:29:04 +0300

    Reset db migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+User.create(name: 'Felix', email: 'felixdev@gmail.com', password: 'test12345')
+icon = fixture_file_upload(Rails.root.join('spec/fixtures/icons/uber.png'), 'image/png')
+Category.create(name: 'Travel', icon:, user_id: @user.id)
+Transaction.create(name: 'Nairobi', amount: 380, author_id:  @user.id, category_id: @category.id)
